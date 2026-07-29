069fc77a43104f8d75073c1b0846768a5080b168
Khadijah Alghamdi <kalghmedi@gmail.com>
2026-07-29 15:00:59 +0000

    Add function that prints a triangle

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_triangle - Prints a right-aligned triangle
+ * @size: The size of the triangle
+ *
+ * Return: Nothing
+ */
+void print_triangle(int size)
+{
+	int row;
+	int space;
+	int hash;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+
+	for (row = 1; row <= size; row++)
+	{
+		for (space = 0; space < size - row; space++)
+			_putchar(' ');
+
+		for (hash = 0; hash < row; hash++)
+			_putchar('#');
+
+		_putchar('\n');
+	}
+}
