d51a149929dad5118a21588423635b633b46844a
Khadijah Alghamdi <kalghmedi@gmail.com>
2026-07-30 14:05:50 +0000

    Add calculator menu loop and validation

--- simple-calculator/calculator.c
+++ simple-calculator/calculator.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - Displays a calculator menu and allows the user to quit
+ * main - Displays a calculator menu until the user quits
  *
  * Return: Always 0
  */
@@ -10,17 +10,23 @@ int main(void)
 	int choice;
 
 	printf("Simple Calculator\n");
-	printf("1) Add\n");
-	printf("2) Subtract\n");
-	printf("3) Multiply\n");
-	printf("4) Divide\n");
-	printf("0) Quit\n");
-	printf("Choice: ");
 
-	scanf("%d", &choice);
+	do {
+		printf("1) Add\n");
+		printf("2) Subtract\n");
+		printf("3) Multiply\n");
+		printf("4) Divide\n");
+		printf("0) Quit\n");
+		printf("Choice: ");
 
-	if (choice == 0)
-		printf("Bye!\n");
+		scanf("%d", &choice);
+
+		if (choice < 0 || choice > 4)
+			printf("Invalid choice\n");
+
+	} while (choice != 0);
+
+	printf("Bye!\n");
 
 	return (0);
 }
