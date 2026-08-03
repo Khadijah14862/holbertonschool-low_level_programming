389c719b228ca1b4364ff04c9f268c0944c7b1e1
Khadijah Alghamdi <kalghmedi@gmail.com>
2026-08-03 14:40:20 +0000

    Add _memcpy function

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -25,4 +25,5 @@ char *cap_string(char *);
 int is_separator(char c);
 char *leet(char *);
 char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
 #endif
