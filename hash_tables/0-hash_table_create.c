446cd9d88102623d8dcc5d2f70857a0616fc4a9e
Khadijah Alghamdi <kalghmedi@gmail.com>
2026-08-12 15:13:33 +0000

    Add hash table create function

---
+++ hash_tables/0-hash_table_create.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - Creates a hash table
+ * @size: Size of the hash table array
+ *
+ * Return: Pointer to the new hash table, or NULL on failure
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *table;
+	unsigned long int i;
+
+	if (size == 0)
+		return (NULL);
+
+	table = malloc(sizeof(hash_table_t));
+	if (table == NULL)
+		return (NULL);
+
+	table->array = malloc(sizeof(hash_node_t *) * size);
+	if (table->array == NULL)
+	{
+		free(table);
+		return (NULL);
+	}
+
+	table->size = size;
+
+	for (i = 0; i < size; i++)
+		table->array[i] = NULL;
+
+	return (table);
+}
---
+++ hash_tables/README.md
@@ -0,0 +1,3 @@
+# Hash Tables
+
+This project covers hash functions, hash tables, and collision handling in C.
