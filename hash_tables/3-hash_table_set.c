d1b493bcfcaa0235106c2c86a0467c15811ec6c4
Khadijah Alghamdi <kalghmedi@gmail.com>
2026-08-12 15:22:31 +0000

    Fix Betty style for hash table set

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -3,6 +3,60 @@
 #include "hash_tables.h"
 
 /**
+ * update_node - Updates the value of an existing node
+ * @node: Node to update
+ * @value: New value
+ *
+ * Return: 1 on success, 0 on failure
+ */
+static int update_node(hash_node_t *node, const char *value)
+{
+	char *new_value;
+
+	new_value = strdup(value);
+	if (new_value == NULL)
+		return (0);
+
+	free(node->value);
+	node->value = new_value;
+
+	return (1);
+}
+
+/**
+ * create_node - Creates a new hash table node
+ * @key: Key to duplicate
+ * @value: Value to duplicate
+ *
+ * Return: Pointer to new node, or NULL on failure
+ */
+static hash_node_t *create_node(const char *key, const char *value)
+{
+	hash_node_t *new;
+
+	new = malloc(sizeof(hash_node_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->key = strdup(key);
+	if (new->key == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+
+	new->value = strdup(value);
+	if (new->value == NULL)
+	{
+		free(new->key);
+		free(new);
+		return (NULL);
+	}
+
+	return (new);
+}
+
+/**
  * hash_table_set - Adds or updates an element in a hash table
  * @ht: Hash table
  * @key: Key
@@ -15,7 +69,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index;
 	hash_node_t *node;
 	hash_node_t *new;
-	char *new_value;
 
 	if (ht == NULL || key == NULL || value == NULL || key[0] == '\0')
 		return (0);
@@ -26,36 +79,13 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	while (node != NULL)
 	{
 		if (strcmp(node->key, key) == 0)
-		{
-			new_value = strdup(value);
-			if (new_value == NULL)
-				return (0);
-
-			free(node->value);
-			node->value = new_value;
-			return (1);
-		}
+			return (update_node(node, value));
 		node = node->next;
 	}
 
-	new = malloc(sizeof(hash_node_t));
+	new = create_node(key, value);
 	if (new == NULL)
 		return (0);
-
-	new->key = strdup(key);
-	if (new->key == NULL)
-	{
-		free(new);
-		return (0);
-	}
-
-	new->value = strdup(value);
-	if (new->value == NULL)
-	{
-		free(new->key);
-		free(new);
-		return (0);
-	}
 
 	new->next = ht->array[index];
 	ht->array[index] = new;
