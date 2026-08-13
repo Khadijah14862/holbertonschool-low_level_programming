4992df24bf314361bc105d0d2fbf872a90a6dba2
Khadijah Alghamdi <kalghmedi@gmail.com>
2026-08-13 18:32:08 +0000

    Fix safe session data updates

--- secure_data_handling/session.c
+++ secure_data_handling/session.c
@@ -46,14 +46,12 @@ int session_set_data(session_t *s, const unsigned char *data, size_t data_len)
                 s->data_len = 0;
                 return 1;
         }
-       tmp = (unsigned char *)realloc(s->data, data_len);
-        s->data = tmp;
+        tmp = (unsigned char *)realloc(s->data, data_len);
 
-if (!s->data) {
-        free(s->id);
-        free(s);
-        return 0;
-}
+        if (!tmp)
+                return 0;
+
+        s->data = tmp;
 
         memcpy(s->data, data, data_len);
         s->data_len = data_len;
