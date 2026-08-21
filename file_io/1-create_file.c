7ffdf011ec911342e2704130b549845af8bccbc6
Khadijah Alghamdi <kalghmedi@gmail.com>
2026-08-21 16:00:30 +0000

    Add create_file function

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -13,33 +13,32 @@ int create_file(const char *filename, char *text_content)
 {
 	int fd;
 	ssize_t written;
-	size_t len = 0;
+	int len = 0;
 
 	if (filename == NULL)
 		return (-1);
-
-	if (text_content != NULL)
-	{
-		while (text_content[len])
-			len++;
-	}
 
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
 
-	if (text_content != NULL)
+	if (text_content == NULL)
 	{
-		written = write(fd, text_content, len);
-		if (written == -1 || written != (ssize_t)len)
-		{
-			close(fd);
-			return (-1);
-		}
+		close(fd);
+		return (1);
 	}
 
-	if (close(fd) == -1)
+	while (text_content[len] != '\0')
+		len++;
+
+	written = write(fd, text_content, len);
+	if (written != len)
+	{
+		close(fd);
 		return (-1);
+	}
+
+	close(fd);
 
 	return (1);
 }
